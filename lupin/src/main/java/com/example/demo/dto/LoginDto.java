62ed2464ec10f655f739dc581b2f694e2e6625b5
parkil625 <tomaberry@naver.com>
2025-11-23 23:03:00 +0900

    로그인 수정

--- lupin/src/main/java/com/example/demo/dto/LoginDto.java
+++ lupin/src/main/java/com/example/demo/dto/LoginDto.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 @Builder
 public class LoginDto {
     private Long id;        // 유저 DB PK (user_id)
-    private String email;   // 로그인 아이디 (userId/email)
+    private String userId;
+    private String email;
     private String name;    // 사용자 실명
     private String role;    // 권한 (ROLE_USER 등)
 
     private String accessToken;
     private String refreshToken;
+
 }

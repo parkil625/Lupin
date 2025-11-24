dec815c96bf2a468df5c2d7c7ccf258a55e3b7a2
parkil625 <tomaberry@naver.com>
2025-11-24 16:44:20 +0900

    fix: 테스트 환경 설정 완료 (프로퍼티, 프로필, 환경변수)

--- lupin/src/test/java/com/example/demo/repository/FeedRepositoryTest.java
+++ lupin/src/test/java/com/example/demo/repository/FeedRepositoryTest.java
@@ -12,11 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class FeedRepositoryTest {
 

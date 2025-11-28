af0eb67c12aeb4f76d0b4d2defa500987eee7b96
Jellyking12 <pfielskdh46@gmail.com>
2025-11-28 17:53:43 +0900

    TDD 작성 - chat

--- lupin/src/main/java/com/example/demo/dto/request/ChatMessageRequest.java
+++ lupin/src/main/java/com/example/demo/dto/request/ChatMessageRequest.java
@@ -1,14 +1,27 @@
 package com.example.demo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+/**
+ * 채팅 메시지 전송 요청 DTO
+ */
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ChatMessageRequest {
+
+    @NotBlank(message = "채팅방 ID는 필수입니다.")
     private String roomId;
+
+    @NotNull(message = "발신자 ID는 필수입니다.")
     private Long senderId;
+
+    @NotBlank(message = "메시지 내용은 필수입니다.")
     private String content;
 }

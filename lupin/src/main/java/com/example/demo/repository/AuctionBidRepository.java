d65a305f29a93e965766919443be112abe9d3e78
parkil625 <tomaberry@naver.com>
2025-11-30 00:45:13 +0900

    fix: add generic types to AuctionBidRepository

--- lupin/src/main/java/com/example/demo/repository/AuctionBidRepository.java
+++ lupin/src/main/java/com/example/demo/repository/AuctionBidRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
+import com.example.demo.domain.entity.AuctionBid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionBidRepository extends JpaRepository {
+public interface AuctionBidRepository extends JpaRepository<AuctionBid, Long> {
 }

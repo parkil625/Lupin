51c5ec083b38533aa0702a3c627807a8b2583ed0
bbananana <chdjehong@naver.com>
2025-12-05 16:29:26 +0900

    예정인 경매 조회

--- lupin/src/main/java/com/example/demo/controller/AuctionContoller.java
+++ lupin/src/main/java/com/example/demo/controller/AuctionContoller.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.response.OngoingAuctionResponse;
+import com.example.demo.dto.response.ScheduledAuctionResponse;
 import com.example.demo.service.AuctionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,5 +23,13 @@ public class AuctionContoller {
         // 보여주신 서비스 메서드를 호출합니다.
         return ResponseEntity.ok(auctionService.getOngoingAuctionWithItem());
     }
+
+    @GetMapping("/scheduled")
+    public ResponseEntity<List<ScheduledAuctionResponse>> getScheduledAuction() {
+        // 보여주신 서비스 메서드를 호출합니다.
+        return ResponseEntity.ok(auctionService.scheduledAuctionWithItem());
+    }
+
+
 
 }

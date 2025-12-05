38cbcd6795f9f6e744f63b6597ef83bc0053fd93
parkil625 <tomaberry@naver.com>
2025-12-05 12:30:44 +0900

    fix: 스토리북 import 경로 및 피드 상세 컨텐츠 영역 수정

--- lupin/src/components/pages/Pages.stories.tsx
+++ lupin/src/components/pages/Pages.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react';
 import { useState } from 'react';
 
 // 페이지 컴포넌트

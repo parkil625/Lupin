d0f44e40a20bf1e10d9c40267b8c8ac763a7c607
parkil625 <tomaberry@naver.com>
2025-12-06 19:58:09 +0900

    fix: 피드 카드 반응형 max-width 추가 - 좁은 화면에서 넘침 방지

--- lupin/src/components/dashboard/feed/Feed.tsx
+++ lupin/src/components/dashboard/feed/Feed.tsx
@@ -526,7 +526,7 @@ function FeedItem({
   return (
     <div className={`h-full max-h-[calc(100vh-130px)] md:max-h-full w-fit mx-auto flex shadow-xl rounded-2xl overflow-hidden transition-all duration-300 relative`}>
       {/* 피드 카드 (왼쪽) */}
-      <div className="h-full aspect-[9/16] flex flex-col flex-shrink-0">
+      <div className="h-full aspect-[9/16] max-w-[calc(100vw-32px)] flex flex-col flex-shrink-0">
         {/* 이미지 영역 - 57% */}
         <div className="relative h-[57%]">
           {hasImages ? (

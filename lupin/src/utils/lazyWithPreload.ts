bbc35c80eb5936a931958d571313b5efb4952c18
parkil625 <tomaberry@naver.com>
2025-12-09 11:20:36 +0900

    fix: lazyWithPreload eslint-disable 적용

--- lupin/src/utils/lazyWithPreload.ts
+++ lupin/src/utils/lazyWithPreload.ts
@@ -1,7 +1,7 @@
-import { lazy, LazyExoticComponent } from "react";
+import { lazy, ComponentType, LazyExoticComponent } from "react";
 
-// React.lazy와 동일한 타입 시그니처 사용
-type LazyComponentWithPreload<T extends React.ComponentType<never>> = LazyExoticComponent<T> & {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type LazyComponentWithPreload<T extends ComponentType<any>> = LazyExoticComponent<T> & {
   preload: () => Promise<{ default: T }>;
 };
 
@@ -9,10 +9,11 @@ type LazyComponentWithPreload<T extends React.ComponentType<never>> = LazyExotic
  * lazy + preload 기능을 가진 동적 import 유틸리티
  * 마우스 hover 시 미리 다운로드를 시작하여 클릭 시 즉시 렌더링 가능
  */
-export function lazyWithPreload<T extends React.ComponentType<never>>(
+export function lazyWithPreload<T extends ComponentType<any>>(
   factory: () => Promise<{ default: T }>
 ): LazyComponentWithPreload<T> {
   const Component = lazy(factory) as LazyComponentWithPreload<T>;
   Component.preload = factory;
   return Component;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */

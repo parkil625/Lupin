e71a76013f48b1700983967ce0a4184384477223
Jellyking12 <pfielskdh46@gmail.com>
2025-12-10 09:28:15 +0900

    500 오류 재도전

--- lupin/src/main/java/com/example/demo/service/AppointmentService.java
+++ lupin/src/main/java/com/example/demo/service/AppointmentService.java
@@ -9,6 +9,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import com.example.demo.exception.BusinessException;
 
 import java.util.List;
 

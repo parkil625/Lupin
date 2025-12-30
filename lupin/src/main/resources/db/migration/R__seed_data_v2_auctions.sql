19053c82bcd97d1683e74ea176b7c3784f3ae925
bbananana <chdjehong@naver.com>
2025-12-30 10:57:46 +0900

    경매 데이터 재배포

--- lupin/src/main/resources/db/migration/R__seed_data_v2_auctions.sql
+++ lupin/src/main/resources/db/migration/R__seed_data_v2_auctions.sql
@@ -54,7 +54,7 @@ INSERT INTO auctions (
 ) VALUES (
              0,
              @now - INTERVAL 1 DAY,
-             @now + INTERVAL 8 HOUR,
+             @now + INTERVAL 9 HOUR,
              false,
              30,
              'ACTIVE',

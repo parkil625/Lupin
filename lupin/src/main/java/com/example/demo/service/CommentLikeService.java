7a3a13ab13d814d52c4456a1741c9956423446be
parkil625 <tomaberry@naver.com>
2025-12-31 09:33:17 +0900

    로그 어노테이션 추가

--- lupin/src/main/java/com/example/demo/service/CommentLikeService.java
+++ lupin/src/main/java/com/example/demo/service/CommentLikeService.java
@@ -11,12 +11,14 @@ import com.example.demo.repository.CommentRepository;
 import com.example.demo.repository.NotificationRepository;
 import com.example.demo.domain.enums.NotificationType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)

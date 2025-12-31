697cd8a50090cdd1184e1c6577bfad89b866af97
parkil625 <tomaberry@naver.com>
2025-12-31 10:51:31 +0900

    에러 해결

--- lupin/src/main/java/com/example/demo/controller/NotificationSseController.java
+++ lupin/src/main/java/com/example/demo/controller/NotificationSseController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController

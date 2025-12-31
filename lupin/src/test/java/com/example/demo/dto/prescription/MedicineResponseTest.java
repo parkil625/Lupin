44c8f004a5df87b69477cb4c190730fe22976f5d
Jellyking12 <pfielskdh46@gmail.com>
2025-12-31 11:23:39 +0900

    확인용

--- lupin/src/test/java/com/example/demo/dto/prescription/MedicineResponseTest.java
+++ lupin/src/test/java/com/example/demo/dto/prescription/MedicineResponseTest.java
@@ -15,10 +15,8 @@ class MedicineResponseTest {
         // given
         Medicine medicine = Medicine.builder()
                 .id(1L)
-                .code("A01BC01")
                 .name("타이레놀")
                 .description("해열진통제")
-                .precautions("공복 시 복용 주의")
                 .build();
 
         // when
@@ -26,10 +24,8 @@ class MedicineResponseTest {
 
         // then
         assertThat(response.getId()).isEqualTo(1L);
-        assertThat(response.getCode()).isEqualTo("A01BC01");
         assertThat(response.getName()).isEqualTo("타이레놀");
         assertThat(response.getDescription()).isEqualTo("해열진통제");
-        assertThat(response.getPrecautions()).isEqualTo("공복 시 복용 주의");
     }
 
     @Test
@@ -38,10 +34,8 @@ class MedicineResponseTest {
         // given
         Medicine medicine = Medicine.builder()
                 .id(1L)
-                .code("A01BC01")
                 .name("타이레놀")
                 .description(null)
-                .precautions("공복 시 복용 주의")
                 .build();
 
         // when
@@ -49,52 +43,7 @@ class MedicineResponseTest {
 
         // then
         assertThat(response.getId()).isEqualTo(1L);
-        assertThat(response.getCode()).isEqualTo("A01BC01");
+        assertThat(response.getName()).isEqualTo("타이레놀");
         assertThat(response.getDescription()).isNull();
-        assertThat(response.getPrecautions()).isEqualTo("공복 시 복용 주의");
-    }
-
-    @Test
-    @DisplayName("주의사항이 null인 경우 처리")
-    void from_WithNullPrecautions_ShouldHandleGracefully() {
-        // given
-        Medicine medicine = Medicine.builder()
-                .id(1L)
-                .code("A01BC01")
-                .name("타이레놀")
-                .description("해열진통제")
-                .precautions(null)
-                .build();
-
-        // when
-        MedicineResponse response = MedicineResponse.from(medicine);
-
-        // then
-        assertThat(response.getId()).isEqualTo(1L);
-        assertThat(response.getCode()).isEqualTo("A01BC01");
-        assertThat(response.getDescription()).isEqualTo("해열진통제");
-        assertThat(response.getPrecautions()).isNull();
-    }
-
-    @Test
-    @DisplayName("설명과 주의사항 모두 null인 경우 처리")
-    void from_WithBothDescriptionAndPrecautionsNull_ShouldHandleGracefully() {
-        // given
-        Medicine medicine = Medicine.builder()
-                .id(1L)
-                .code("A01BC01")
-                .name("타이레놀")
-                .description(null)
-                .precautions(null)
-                .build();
-
-        // when
-        MedicineResponse response = MedicineResponse.from(medicine);
-
-        // then
-        assertThat(response.getId()).isEqualTo(1L);
-        assertThat(response.getCode()).isEqualTo("A01BC01");
-        assertThat(response.getDescription()).isNull();
-        assertThat(response.getPrecautions()).isNull();
     }
 }
